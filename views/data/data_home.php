635dc3c48ecbdba2a70634ccbeac3687879bffd6
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 15:05:30 +0700

    Create data_home.php

---
+++ views/data/data_home.php
@@ -0,0 +1,26 @@
+<div class="col-xl-3 flex-column d-flex grid-margin stretch-card">
+    <div class="row flex-grow">
+        <div class="col-sm-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title animated fadeInDown mb-4">Total Perjalanan Anda</h4>
+                    <h4 class="text-dark font-weight-bold mb-2 animated fadeInUp">0</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title animated fadeInDown mb-4">Perjalanan Hari Ini</h4>
+                    <h4 class="text-dark font-weight-bold mb-2 animated fadeInUp">0</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-xl-9 d-flex grid-margin stretch-card">
+    <div class="card">
+        <div class="card-body animated fadeIn" style="background-image: url('assets/images/travel.jpg'); background-size: cover; border-radius: 20px; background-position: center;">
+        </div>
+    </div>
+</div>
