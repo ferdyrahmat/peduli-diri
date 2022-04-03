3cbe2402fc977b94d53864507db537975dd81cbb
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 10:08:42 +0700

    Create register.php

---
+++ views/auth/register.php
@@ -0,0 +1,32 @@
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth px-0">
+            <div class="row w-100 mx-0">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                        <div class="brand-logo">
+                            <img src="assets/images/logo-dark.svg" alt="logo">
+                        </div>
+                        <h6 class="font-weight-light text-center">Signing up is easy. It only takes a few steps</h6>
+                        <form class="pt-3" action="proses-daftar" method="POST" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <input type="number" class="form-control form-control-lg" name="nik" id="nikreg" placeholder="Nomor Induk Kependudukan" autocomplete="off" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" name="nama" id="namareg" placeholder="Nama Lengkap">
+                            </div>
+                            <div class="mt-3">
+                                <button class="btn btn-block btn-info btn-lg font-weight-medium auth-form-btn">Daftar</button>
+                            </div>
+                            <div class="text-center mt-4 font-weight-light">
+                                Sudah mempunyai akun ? <a href="masuk" class="text-info">Masuk Sekarang</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
