74a0558d9bc2dc7191c1ca7c158afd7990f45c83
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 15:14:13 +0700

    Update footer_layout.php

--- layout/footer_layout.php
+++ layout/footer_layout.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Hak Cipta &copy; <?= date('Y'); ?> PeduliDiri.</span>
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Hak Cipta &copy; <?= date('Y'); ?> | Catatan Perjalanan ( PeduliDiri ).</span>
         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Versi 1.0</span>
     </div>
 </footer>
