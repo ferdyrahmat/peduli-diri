452b56f49bf515cd567526cb31e24ee96b2d3305
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 15:05:33 +0700

    Create data_travel.php

---
+++ views/data/data_travel.php
@@ -0,0 +1,29 @@
+<table class="table table-striped table-bordered" style="width: 100%;" id="dataTravel">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Tanggal</th>
+            <th>Jam</th>
+            <th>Lokasi</th>
+            <th>Suhu</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Example Data</td>
+            <td>Example Data</td>
+            <td>Example Data</td>
+            <td>Example Data</td>
+            <td>Example Data</td>
+            <td>Example Data</td>
+        </tr>
+    </tbody>
+</table>
+
+
+<script>
+    $(document).ready(function() {
+        $('#dataTravel').DataTable();
+    });
+</script>
