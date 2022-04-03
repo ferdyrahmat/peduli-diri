90cc9a546de1ae4547136447495898082c3f80fc
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 14:21:50 +0700

    Update sidebar_layout.php

--- layout/sidebar_layout.php
+++ layout/sidebar_layout.php
@@ -1,79 +1,38 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <div class="user-profile">
         <div class="user-image">
-            <img src="images/faces/face28.png">
+            <img src="assets/images/default-user-icon.jpg">
         </div>
         <div class="user-name">
-            Edward Spencer
+            <?= $_SESSION['nama']; ?>
         </div>
         <div class="user-designation">
-            Developer
+            <?= $_SESSION['nik']; ?>
         </div>
     </div>
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="index.html">
-                <i class="icon-box menu-icon"></i>
-                <span class="menu-title">Dashboard</span>
+            <a class="nav-link" href="home">
+                <i class="fa fa-home menu-icon"></i>
+                <span class="menu-title">Home</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="icon-disc menu-icon"></i>
-                <span class="menu-title">UI Elements</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Typography</a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="pages/forms/basic_elements.html">
-                <i class="icon-file menu-icon"></i>
-                <span class="menu-title">Form elements</span>
+            <a class="nav-link" href="catatan-perjalanan">
+                <i class="fa fa-book menu-icon"></i>
+                <span class="menu-title">Catatan Perjalanan</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="pages/charts/chartjs.html">
-                <i class="icon-pie-graph menu-icon"></i>
-                <span class="menu-title">Charts</span>
+            <a class="nav-link" href="tambah-catatan">
+                <i class="fa fa-plus-square menu-icon"></i>
+                <span class="menu-title">Tambah Catatan</span>
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="pages/tables/basic-table.html">
-                <i class="icon-command menu-icon"></i>
-                <span class="menu-title">Tables</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="pages/icons/feather-icons.html">
-                <i class="icon-help menu-icon"></i>
-                <span class="menu-title">Icons</span>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="icon-head menu-icon"></i>
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
-                    <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
-                </ul>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="docs/documentation.html">
-                <i class="icon-book menu-icon"></i>
-                <span class="menu-title">Documentation</span>
+            <a class="nav-link" href="keluar">
+                <i class="fa fa-sign-out menu-icon"></i>
+                <span class="menu-title">Keluar</span>
             </a>
         </li>
     </ul>
