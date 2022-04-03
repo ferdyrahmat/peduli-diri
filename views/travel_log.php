d620129891607efc21489435ca718eee90d39c38
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 15:14:00 +0700

    Create travel_log.php

---
+++ views/travel_log.php
@@ -0,0 +1,37 @@
+<div class="content-wrapper">
+    <div class="row">
+        <div class="col-sm-12 mb-4 mb-xl-0">
+            <h4 class="font-weight-bold text-dark">Catatan Perjalanan</h4>
+        </div>
+    </div>
+    <div class="row mt-2">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body" id="loadDataTravel">
+                    <table class="table table-striped table-bordered" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Tanggal</th>
+                                <th>Jam</th>
+                                <th>Lokasi</th>
+                                <th>Suhu</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Example Data</td>
+                                <td>Example Data</td>
+                                <td>Example Data</td>
+                                <td>Example Data</td>
+                                <td>Example Data</td>
+                                <td>Example Data</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
