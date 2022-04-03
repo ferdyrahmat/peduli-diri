d2c3eb19ddcf2b9ac4231995e64b07f9be93824b
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 07:11:04 +0700

    Create login.php

---
+++ views/auth/login.php
@@ -0,0 +1,32 @@
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth px-0">
+            <div class="row w-100 mx-0">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                        <div class="brand-logo">
+                            <img src="assets/images/logo-dark.svg" alt="logo">
+                        </div>
+                        <h6 class="font-weight-light text-center">Sign in to continue.</h6>
+                        <form class="pt-3">
+                            <div class="form-group">
+                                <input type="number" class="form-control form-control-lg" name="nik" id="nik" placeholder="Nomor Induk Kependudukan" onwheel="this.blur()" autocomplete="off">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" name="nama" id="nama" placeholder="Nama Lengkap" readonly>
+                            </div>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-block btn-info btn-lg font-weight-medium auth-form-btn" id="btn">Masuk</button>
+                            </div>
+                        </form>
+                        <div class="text-center mt-4 font-weight-light">
+                            Belum mempunyai akun ? <a href="daftar" class="text-info">Daftar Sekarang</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+</div>
