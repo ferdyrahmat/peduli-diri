d393237780b5a915b64c64bb12baff603e46af2a
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-03 10:38:44 +0700

    Update main.php

--- main.php
+++ main.php
@@ -6,20 +6,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Regal Admin</title>
-    <!-- base:css -->
     <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="assets/vendors/feather/feather.css">
     <link rel="stylesheet" href="assets/vendors/base/vendor.bundle.base.css">
-    <!-- endinject -->
-    <!-- plugin css for this page -->
     <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css" />
     <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/vendors/jquery-bar-rating/fontawesome-stars-o.css">
     <link rel="stylesheet" href="assets/vendors/jquery-bar-rating/fontawesome-stars.css">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
     <link rel="stylesheet" href="assets/css/style.css">
-    <!-- endinject -->
     <link rel="shortcut icon" href="images/favicon.png" />
 </head>
 
