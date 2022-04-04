26d75ffc57f6260c96993430e6a5e67aaecf9c21
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-04 09:13:47 +0700

    Update sidebar_layout.php

--- layout/sidebar_layout.php
+++ layout/sidebar_layout.php
@@ -30,10 +30,31 @@
             </a>
         </li>
         <li class="nav-item mt-4">
-            <a class="nav-link" href="keluar">
+            <a class="nav-link" href="#" data-target="#logoutModal" data-toggle="modal">
                 <i class="fa fa-sign-out menu-icon"></i>
                 <span class="menu-title">Keluar</span>
             </a>
         </li>
     </ul>
 </nav>
+
+<!-- Modal Logout -->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <a href="keluar" class="btn btn-danger">Logout</a>
+            </div>
+        </div>
+    </div>
+</div>
