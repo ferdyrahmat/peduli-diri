9d0348e7df922a0fc78f8219fa2b02f8335ac9c9
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-04 14:26:37 +0700

    Update sidebar_layout.php

--- layout/sidebar_layout.php
+++ layout/sidebar_layout.php
@@ -44,9 +44,9 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
-                </button>
+                </button> -->
             </div>
             <div class="modal-body">
                 ...
