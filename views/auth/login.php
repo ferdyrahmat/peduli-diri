891c8144d21a88846791d07094e973e205933578
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-04 09:12:02 +0700

    Update login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -3,11 +3,11 @@
         <div class="content-wrapper d-flex align-items-center auth px-0">
             <div class="row w-100 mx-0">
                 <div class="col-lg-4 mx-auto">
-                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                    <div class="auth-form-light text-center py-5 px-4 px-sm-5">
                         <div class="brand-logo">
-                            <img src="assets/images/logo-dark.svg" alt="logo">
+                            <img src="assets/images/pedulidiri-auth.png" alt="logo">
                         </div>
-                        <h6 class="font-weight-light text-center">Sign in to continue.</h6>
+                        <h5 class="font-weight-light text-center">Masuk | PeduliDiri</h5>
                         <form class="pt-3" method="POST" action="proses-masuk" id="login" enctype="multipart/form-data">
                             <div id="notif">
                             </div>
