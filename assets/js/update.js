d2f9e0dccf3b6516adc0cc49cd0e864ee0dc47e2
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-05 13:35:36 +0700

    Create update.js

---
+++ assets/js/update.js
@@ -0,0 +1,39 @@
+/*************************************
+*  Update.Js                     
+*                                  
+*  Created by Ferdy Rahmat R.      
+*  on 5 April 2022                
+*                                  
+*************************************/
+document.title = "PeduliDiri | Edit Catatan Perjalanan";
+
+$(function () {
+    $('#edit-catatan').submit(function (e) {
+        e.preventDefault();
+
+        var data = $(this).serialize();
+
+        $.ajax({
+            url: $(this).attr("action"),
+            method: $(this).attr("method"),
+            data: data,
+            dataType: "JSON",
+            success: function (response) {
+                if (response.status == "success") {
+                    Swal.fire({
+                        title: 'Berhasil',
+                        text: response.msg,
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000
+                    }).then(function () {
+                        window.location = response.redirect;
+                    })
+                }
+            },
+            error: function () {
+
+            }
+        })
+    })
+})
