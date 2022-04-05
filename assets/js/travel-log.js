7fa6e1ca67ceb0d0a3519119ac44128698fdc9ca
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-05 13:35:31 +0700

    Update travel-log.js

--- assets/js/travel-log.js
+++ assets/js/travel-log.js
@@ -1,8 +1,15 @@
+/*************************************
+*  Travel-Log.Js                     
+*                                  
+*  Created by Ferdy Rahmat R.      
+*  on 5 April 2022                
+*                                  
+*************************************/
 document.title = "PeduliDiri | Catatan Perjalanan";
 
 var instance = $('#loadDataTravel').scheletrone({
     url: "data-travel",
     debug: {
-        latency: 3000
+        latency: 2000
     }
 })
