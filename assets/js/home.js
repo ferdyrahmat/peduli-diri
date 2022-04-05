e09570fbcb267644e85bf51fa2f4e9266812cd4d
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-05 13:35:04 +0700

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,15 @@
+/*************************************
+*  Home.Js                     
+*                                  
+*  Created by Ferdy Rahmat R.      
+*  on 5 April 2022                
+*                                  
+*************************************/
 document.title = "PeduliDiri | Home";
 
 var instance = $('#loadDataHome').scheletrone({
     url: "data-home",
     debug: {
-        latency: 3000
+        latency: 2000
     }
 })
