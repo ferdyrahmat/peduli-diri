fd06bc53532b0a1cda4c95a9e9f4dc1cc8c7dd47
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-06 07:44:51 +0700

    Create Register.php

---
+++ function/Register.php
@@ -0,0 +1,35 @@
+<?php
+
+$nik = $_POST['nik'];
+$nama = $_POST['nama'];
+
+$db = file("../database/config.txt", FILE_IGNORE_NEW_LINES);
+
+foreach ($db as $data) {
+    $pd = explode("|", $data);
+    if ($nik == $pd['0']) {
+        $cek = true;
+    }
+}
+
+if ($cek) {
+    $response = [
+        'status'    => 'failed',
+        'msg'       => 'NIK yang anda gunakan telah terdaftar',
+    ];
+
+    echo json_encode($response);
+} else {
+    $data = $nik . "|" . $nama . "\n";
+    $db = fopen("../database/config.txt", 'a');
+
+    fwrite($db, $data);
+    fclose($db);
+
+    $response = [
+        'status'    => 'success',
+        'msg'       => 'Pendaftaran berhasil'
+    ];
+
+    echo json_encode($response);
+}
