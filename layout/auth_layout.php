1112310c545010731016c97d430b0508757cfea5
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-06 08:05:11 +0700

    Update auth_layout.php

--- layout/auth_layout.php
+++ layout/auth_layout.php
@@ -14,13 +14,13 @@ if (isset($_SESSION['IsLogged'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>PeduliDiri</title>
-    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/feather/feather.css">
-    <link rel="stylesheet" href="assets/vendors/base/vendor.bundle.base.css">
-    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="assets/css/animate.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/feather/feather.css">
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/base/vendor.bundle.base.css">
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/css/animate.min.css">
+    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/css/style.css">
+    <link rel="shortcut icon" href="http://localhost/peduli-diri-native/assets/images/favicon.png" />
 </head>
 
 <body>
@@ -42,11 +42,11 @@ if (isset($_SESSION['IsLogged'])) {
 
     ?>
 
-    <script src="assets/vendors/base/vendor.bundle.base.js"></script>
-    <script src="assets/js/off-canvas.js"></script>
-    <script src="assets/js/hoverable-collapse.js"></script>
-    <script src="assets/js/template.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="http://localhost/peduli-diri-native/assets/vendors/base/vendor.bundle.base.js"></script>
+    <script src="http://localhost/peduli-diri-native/assets/js/off-canvas.js"></script>
+    <script src="http://localhost/peduli-diri-native/assets/js/hoverable-collapse.js"></script>
+    <script src="http://localhost/peduli-diri-native/assets/js/template.js"></script>
+    <script src="http://localhost/peduli-diri-native/assets/js/bootstrap.min.js"></script>
 
     <?php
 
