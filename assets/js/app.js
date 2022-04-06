c726a1c71047705f87ef49a9158f0b51465e311a
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-06 07:44:42 +0700

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#infoText', {
+    strings: ["PeduliDiri", "Jaga diri Anda dan Keluarga dari Virus Corona (Covid-19)", "Disipilin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy", "#Indonesia Bisa", "#Indonesia Kuat"],
+    typeSpeed: 50,
+    backDelay: 1000,
+    // startDelay: 500,
+    backSpeed: 50,
+    loop: true,
+});
