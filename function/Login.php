e31c0a926d83af320071f0f55a940d0e26900720
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-08 07:09:10 +0700

    Update Login.php

--- function/Login.php
+++ function/Login.php
@@ -1,21 +1,16 @@
 <?php
-// Memulai Sesi dengan User
 session_start();
 
-// Buat delay selama 3 detik
 sleep('3');
 
 $nik = $_POST['nik'];
 $nama = $_POST['nama'];
 
-// Buat format data user yang tersimpan dalam file config.txt
 $data = $nik . "|" . $nama;
-// Buka file config.txt
+
 $db = file("../database/config.txt", FILE_IGNORE_NEW_LINES);
 
-// Mencari data user di file config.txt dengan array berdasarkan format data user yang telah kita buat
 if (in_array($data, $db)) {
-    // Jika data ditemukan, maka buat session atau sesi untuk user
     $_SESSION['nik']        = $nik;
     $_SESSION['nama']       = $nama;
     $_SESSION['IsLogged']   = true;
@@ -27,7 +22,6 @@ if (in_array($data, $db)) {
 
     echo json_encode($response);
 } else {
-    // Jika data tidak ditemukan, maka alihkan user kembali ke halaman login
     $response = [
         'status'    => 'failed',
         'msg'       => 'Login Gagal!'
