df74fb3ed36e28dffc9516f5b9ab3ae1fbffecf9
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-08 07:10:43 +0700

    Update Check.php

--- function/Check.php
+++ function/Check.php
@@ -3,22 +3,17 @@ session_start();
 
 $nik = $_POST['nik'];
 
-// Cek NIK
 $db = file("../database/config.txt", FILE_IGNORE_NEW_LINES);
 
 foreach ($db as $data) {
-    // Pecah data
     $pd = explode("|", $data);
-    // Jika NIk ada di dalam file config, maka beritahu kalau sudah terdaftar
+
     if ($nik == $pd['0']) {
-        // Beritahu jika kondisi cek dapat berjalan = true
         $cek = true;
-        // Buat variable nama dan beri isi data nama user sesuai dengan nik
         $nama = $pd['1'];
     }
 }
 
-// Jika NIk ditemukan
 if ($cek) {
     $response = [
         'status'    => 'success',
@@ -26,7 +21,6 @@ if ($cek) {
     ];
 
     echo json_encode($response);
-    // Jika NIK tidak ditemukan
 } else {
     $response = [
         'status'   => 'failed',
