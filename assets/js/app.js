bff918690996c7c5184b823a6d4d3a1bc95e6dfd
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-11 13:20:10 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#infoText', {
-    strings: ["PeduliDiri", "Jaga diri Anda dan Keluarga dari Virus Corona (Covid-19)", "Disipilin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy", "#Indonesia Bisa", "#Indonesia Kuat"],
+    strings: ["Jaga diri Anda dan Keluarga dari Virus Corona (Covid-19)", "Disipilin Menerapkan 3M", "Memakai Masker", "Menjaga Jarak", "Mencuci Tangan", "#Stay Safe and Healthy"],
     typeSpeed: 50,
     backDelay: 1000,
     // startDelay: 500,
