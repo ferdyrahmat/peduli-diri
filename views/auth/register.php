37eb4ce1c385aa57e8f1698fedf8e7afd72b84c0
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-22 09:49:56 +0700

    Update register.php

--- views/auth/register.php
+++ views/auth/register.php
@@ -3,11 +3,12 @@
         <div class="content-wrapper d-flex align-items-center auth px-0">
             <div class="row w-100 mx-0">
                 <div class="col-lg-4 mx-auto">
-                    <div class="auth-form-light text-center py-5 px-4 px-sm-5">
+                    <div class="auth-form-light text-left py-5 px-4 px-sm-5">
                         <div class="brand-logo">
-                            <img src="assets/images/pedulidiri-auth.png" alt="logo" style="margin-top: -20px; margin-bottom: -10px;">
+                            <img src="assets/images/icon_auth.png" alt="logo" style="width: 154px; margin-bottom: -20px;">
                         </div>
-                        <h5 class="font-weight-light text-center">Daftar - PeduliDiri</h5>
+                        <h4>New here?</h4>
+                        <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
                         <form class="pt-3" action="proses-daftar" method="POST" enctype="multipart/form-data" id="register">
                             <div class="form-group">
                                 <input type="number" class="form-control form-control-lg" name="nik" id="nikreg" placeholder="Nomor Induk Kependudukan" onKeyPress="if(this.value.length==16) return false;" onpaste="return false" oncut="return false" oncopy="return false" ondrag="return false" ondrop="return false" onwheel="this.blur()" autocomplete="off" required autofocus>
