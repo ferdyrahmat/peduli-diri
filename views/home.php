aaf28af95fbaaf335eed2b91df332e355e4a2dab
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-23 10:41:19 +0700

    Update home.php

--- views/home.php
+++ views/home.php
@@ -55,4 +55,96 @@
             </div>
         </div>
     </div>
+
+    <?php
+    $nik = $_SESSION['nik'];
+
+    $file = "database/catatan_perjalanan.txt";
+    $db = file($file, FILE_IGNORE_NEW_LINES);
+    foreach ($db as $value) {
+        $pd = explode("|", $value);
+        if ($pd['1'] == $nik) {
+            $data[] = $pd['4'];
+        }
+    }
+
+    if (!empty($data)) {
+        start($data);
+    }
+
+    function start($data)
+    {
+        $jenis[] = null;
+        $i = 0;
+        for ($j = 0; $j < count($data); $j++) {
+            $index = array_search($data[$j], $jenis);
+            if ($index == "") {
+                $jenis[$i] = $data[$j];
+                $i++;
+            }
+        }
+
+        search($data, $jenis);
+    }
+
+    function search($data, $data2)
+    {
+        for ($K = 0; $K < count($data2); $K++) {
+            $key['1'] = $data2[$K];
+            $key['2'] = searchData($data, $data2[$K]);
+        }
+        if ($key['2'] >= 2) {
+            showTable(true);
+        }
+    }
+
+    function searchData($data, $dupval)
+    {
+        $nb = 0;
+        foreach ($data as $key => $val) {
+            if ($val == $dupval) {
+                $nb++;
+            }
+        }
+        return $nb;
+    }
+    ?>
+
+    <?php
+    function showTable($val)
+    {
+        if ($val == true) { ?>
+            <div class="row mt-2" id="loadDataVisited">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="alert alert-light font-weight-bold text-dark">
+                                Data Lokasi yang sering dikunjungi oleh Anda
+                            </div>
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Lokasi</th>
+                                        <th>Terakhir Kunjungan</th>
+                                        <th>Total Kunjungan</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Example Data</td>
+                                        <td>Example Data</td>
+                                        <td>Example Data</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <?php
+        } else {
+            echo "NOOO";
+        }
+    }
+    ?>
 </div>
