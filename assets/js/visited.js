8867fe95ce6f807b0f02267178f812da76ed942d
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-23 10:41:10 +0700

    Create visited.js

---
+++ assets/js/visited.js
@@ -0,0 +1,15 @@
+/*************************************
+*  Visited.Js                     
+*                                  
+*  Created by Ferdy Rahmat R.      
+*  on 5 April 2022                
+*                                  
+*************************************/
+document.title = "PeduliDiri | Home";
+
+var instance = $('#loadDataVisited').scheletrone({
+    url: "data-visited",
+    debug: {
+        latency: 2000
+    }
+})
