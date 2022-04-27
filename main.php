d4e6835c3fbbe71a09638c006c31a3abca9d3ac6
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-27 07:14:48 +0700

    Update main.php

--- main.php
+++ main.php
@@ -14,21 +14,24 @@ if (empty($_SESSION['IsLogged'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>PeduliDiri</title>
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/feather/feather.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/base/vendor.bundle.base.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/flag-icon-css/css/flag-icon.min.css" />
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/jquery-bar-rating/fontawesome-stars-o.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/jquery-bar-rating/fontawesome-stars.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/jquery.skeleton.loader/dist/jquery.skeleton.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/css/animate.min.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/DataTables/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/vendors/gijgo/css/gijgo.min.css">
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/css/jquery-ui.css">
-
-    <link rel="stylesheet" href="http://localhost/peduli-diri-native/assets/css/style.css">
-    <link rel="shortcut icon" href="http://localhost/peduli-diri-native/assets/images/icon_app.png" />
+    <!-- Base href -->
+    <base href="http://localhost/peduli-diri/">
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/vendors/feather/feather.css">
+    <link rel="stylesheet" href="assets/vendors/base/vendor.bundle.base.css">
+    <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css" />
+    <link rel="stylesheet" href="assets/vendors/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/vendors/jquery-bar-rating/fontawesome-stars-o.css">
+    <link rel="stylesheet" href="assets/vendors/jquery-bar-rating/fontawesome-stars.css">
+    <link rel="stylesheet" href="assets/vendors/jquery.skeleton.loader/dist/jquery.skeleton.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/vendors/DataTables/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="assets/vendors/gijgo/css/gijgo.min.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- Shortcut Icon -->
+    <link rel="shortcut icon" href="assets/images/icon_app.png" />
 </head>
 
 <body>
@@ -47,22 +50,23 @@ if (empty($_SESSION['IsLogged'])) {
         </div>
     </div>
 
-    <script src="http://localhost/peduli-diri-native/assets/vendors/base/vendor.bundle.base.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/off-canvas.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/hoverable-collapse.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/template.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/dashboard.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/jquery.skeleton.loader/dist/jquery.scheletrone.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/DataTables/js/jquery.dataTables.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/DataTables/js/dataTables.bootstrap4.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/sweetalert2/package/dist/sweetalert2.all.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/logout.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/typed.js/lib/typed.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/app.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/vendors/gijgo/js/gijgo.min.js"></script>
-    <script src="http://localhost/peduli-diri-native/assets/js/jquery-ui.js"></script>
+    <!-- Script -->
+    <script src="assets/vendors/base/vendor.bundle.base.js"></script>
+    <script src="assets/js/off-canvas.js"></script>
+    <script src="assets/js/hoverable-collapse.js"></script>
+    <script src="assets/js/template.js"></script>
+    <script src="assets/vendors/chart.js/Chart.min.js"></script>
+    <script src="assets/vendors/jquery-bar-rating/jquery.barrating.min.js"></script>
+    <script src="assets/js/dashboard.js"></script>
+    <script src="assets/vendors/jquery.skeleton.loader/dist/jquery.scheletrone.js"></script>
+    <script src="assets/vendors/DataTables/js/jquery.dataTables.min.js"></script>
+    <script src="assets/vendors/DataTables/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/vendors/sweetalert2/package/dist/sweetalert2.all.min.js"></script>
+    <script src="assets/js/logout.js"></script>
+    <script src="assets/vendors/typed.js/lib/typed.js"></script>
+    <script src="assets/js/app.js"></script>
+    <script src="assets/vendors/gijgo/js/gijgo.min.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
 
     <script>
         $("#datepicker").datepicker({
@@ -79,14 +83,14 @@ if (empty($_SESSION['IsLogged'])) {
 
     <?php
     if ($_GET['page'] == 'home') { ?>
-        <script src="http://localhost/peduli-diri-native/assets/js/home.js"></script>
-        <script src="http://localhost/peduli-diri-native/assets/js/visited.js"></script>
+        <script src="assets/js/home.js"></script>
+        <script src="assets/js/visited.js"></script>
     <?php
     } else if ($_GET['page'] == 'catatan-perjalanan') { ?>
-        <script src="http://localhost/peduli-diri-native/assets/js/travel-log.js"></script>
+        <script src="assets/js/travel-log.js"></script>
     <?php
     } else if ($_GET['page'] == 'tambah-catatan') { ?>
-        <script src="http://localhost/peduli-diri-native/assets/js/insert.js"></script>
+        <script src="assets/js/insert.js"></script>
     <?php
     }
     ?>
