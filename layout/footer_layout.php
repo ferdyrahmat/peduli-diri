3d10b241ecb60395942a2277a9a2c8dda8617ef1
Ferdy <83628349+ferdyrahmat@users.noreply.github.com>
2022-04-28 08:24:33 +0700

    Update footer_layout.php

--- layout/footer_layout.php
+++ layout/footer_layout.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Hak Cipta &copy; <?= date('Y'); ?> - PeduliDiri.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Versi 1.0 </span>
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Hak Cipta &copy; <?= date('Y'); ?> - PeduliDiri. Source Code Aplikasi ini tersedia di Github: <a href="https://github.com/ferdyrahmat/peduli-diri" style="color: #4e8ed9;" target="_blank">@ferdyrahmat</a></span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Versi 1.0.1</span>
     </div>
 </footer>
